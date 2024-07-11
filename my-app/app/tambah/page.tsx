18c06170c88238c237f5e6e89742d191d67f32eb
KaitoJoker <22572001@stmik.jayakarta.ac.id>
2024-07-11 20:45:50 +0700

    update

---
+++ my-app/app/tambah/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import MovieForms from "../components/movieForm/movieForms";
+
+export default function tambah() {
+    return (
+        <><br />
+        <div className="container m-auto bg-primary">
+            <MovieForms />
+        </div></>
+    );
+}
--- tic_tac_toe.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-function square ({ value, onSquareClick }) {
-    return (
-        <button className='square' onClick={onSquareClick}>
-            {value}
-        </button>
-    );
-}
